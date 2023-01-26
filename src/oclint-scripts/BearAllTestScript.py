ed304f0e6913a0b6ee006492ccda2591692f7ced
Bruno Sotto-Mayor <machadob@post.bgu.ac.il>
2023-01-26 18:31:28 +0000

    Added test scripts

---
+++ src/oclint-scripts/BearAllTestScript.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+projects_list = ['AbstractFunctionCallFromConstructorTest', 'ComplexMethodTest', 'GodClassTest', 'LongIdentifierTest',
+                 'MultifacetedAbstractionTest', 'UnnecessaryAbstractionTest', 'BrainClassTest',
+                 'HubLikeModularizationTest', 'LongMethodTest', 'RefusedBequestTest', 'UnutilizedAbstractionTest',
+                 'BrainMethodTest', 'DataClassTest', 'ImperativeAbstractionTest', 'LongParameterListTest',
+                 'ShotgunSurgeryTest', 'BrokenModularizationTest', 'DeficientEncapsulationTest',
+                 'InsufficientModularizationTest', 'LongStatementTest', 'SpaghettiCodeTest',
+                 'ClassDataShouldBePrivateTest', 'DispersedCouplingTest', 'IntensiveCouplingTest', 'MagicNumberTest',
+                 'SpeculativeGeneralityTest','ComplexClassTest', 'EmptyCatchClauseTest', 'LargeClassTest',
+                 'MessageChainTest', 'SwissArmyKnifeTest', 'ComplexConditionalTest', 'LazyClassTest',
+                 'MissingDefaultTest', 'UnexploitedEncapsulationTest']
+
+for project in projects_list:
+    print(f'cd /tests-cpp/{project}; bear make')
+
---
+++ src/oclint-scripts/RunAllTestScript.py
@@ -0,0 +1,20 @@
+
+from pathlib import Path
+
+projects_list = ['AbstractFunctionCallFromConstructorTest', 'ComplexMethodTest', 'GodClassTest', 'LongIdentifierTest',
+                 'MultifacetedAbstractionTest', 'UnnecessaryAbstractionTest', 'BrainClassTest',
+                 'HubLikeModularizationTest', 'LongMethodTest', 'RefusedBequestTest', 'UnutilizedAbstractionTest',
+                 'BrainMethodTest', 'DataClassTest', 'ImperativeAbstractionTest', 'LongParameterListTest',
+                 'ShotgunSurgeryTest', 'BrokenModularizationTest', 'DeficientEncapsulationTest',
+                 'InsufficientModularizationTest', 'LongStatementTest', 'SpaghettiCodeTest',
+                 'ClassDataShouldBePrivateTest', 'DispersedCouplingTest', 'IntensiveCouplingTest', 'MagicNumberTest',
+                 'SpeculativeGeneralityTest','ComplexClassTest', 'EmptyCatchClauseTest', 'LargeClassTest',
+                 'MessageChainTest', 'SwissArmyKnifeTest', 'ComplexConditionalTest', 'LazyClassTest',
+                 'MissingDefaultTest', 'UnexploitedEncapsulationTest']
+
+reports_path = Path("/oclint-repo/oclint-scripts/TestsReports")
+reports_path.mkdir(exist_ok=True, parents=True)
+
+for project in projects_list:
+    print(f'/oclint-repo/oclint-scripts/RunRules.sh /tests-cpp/{project}/ > /oclint-repo/oclint-scripts/TestsReports/{project}_report.txt')
+
